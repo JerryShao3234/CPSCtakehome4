6aea3b5d8de38cd1104cd2baddc25004c8c54a03
JerryShao3234 <72317125+JerryShao3234@users.noreply.github.com>
2020-11-12 21:05:59 -0800

    Update recursion.c
    finished Path_Cost and get_maze_dimension functions
    you should still check them though

--- recursion.c
+++ recursion.c
@@ -1,200 +1,310 @@
 /*
- File:              recursion.c
- Purpose:           Exercises for CPSC259 lab 4
- Author:			Robin Yuan, Jerry Shao
- Student #s:		88011879 and 84982321
- CS Accounts:		b9k3b  and c1i3b
- Date:				Nov 13, 2020
+ File:          mazesolver.c
+ Purpose:       Add a brief description
+ Author:			  Your names
+ Student #s:		12345678 and 12345678
+ CS Accounts:		a1a1 and b2b2
+ Date:				  Add the date here
  */
 
- /******************************************************************
-  PLEASE EDIT THIS FILE
+ 
+/******************************************************************
+ PLEASE EDIT THIS FILE AND INCLUDE IN YOUR GRADESCOPE SUBMISSION
 
-  Comments that start with // should be replaced with code
-  Comments that are surrounded by * are hints
-  ******************************************************************/
+ Comments that start with // should be replaced with code
+ Comments that are surrounded by * are hints
+ ******************************************************************/
 
 #define _CRT_SECURE_NO_WARNINGS
-  /* Preprocessor directives */
-#include "recursion.h"
-#include <stdio.h>
+
+/* Preprocessor directives to include libraries */
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include "mazesolver.h"
+
 /*
- Calculates the power.
- PARAM:     base is an integer
- PARAM:     power is an integer
- PRE:       power >= 0
- PRE:       base != 0
- RETURN:    base^power
+ Contains the majority of the maze solver logic
+ PRE:       The file specified by the MACRO in lines 47 and 60 exists in the project
+ POST:      Prints data about maze solutions.
+ RETURN:    None
  */
-int calculate_power(int base, int power)
+void process()
 {
-    if (power == 0) return 1;
-    if (power == 1) return base;
-    return base * calculate_power(base, power - 1);
+  /* Variables */
+  int dimension = 0;
+  FILE* maze_file = NULL;
+  maze_cell** maze = NULL;
+  char outputstring[BUFFER]; // holds output for shortest/cheapest path
+
+  /* Declare variables for holding generated path information */
+  char** paths = NULL;
+  int paths_found = 0;
+
+  /* Opens and parses the maze file.  Replace the first parameter of fopen with
+    different file names defined in the preprocessor section of the header file
+    to test your mazesolver with our sample mazes. */
+  maze_file = fopen(MAZE1, "r");
+
+  if (maze_file) {
+
+    /* Calls the functions that:
+      a) get the size of the maze and stores it in the dimension variable
+      b) copies the maze into memory */
+      // INSERT YOUR CODE HERE (2 lines)
+      dimension = get_maze_dimension(maze_file);
+      maze = parse_maze(maze_file, dimension);
+
+  }
+  else {
+    fprintf(stderr, "Unable to parse maze file: %s\n", MAZE1);
+    system("pause");
+  }
+
+  /* Traverses maze and generates all solutions */
+  // INSERT YOUR CODE HERE (1 line)
+  //generate_all_paths(
+
+  /* Calculates and displays required data */
+  // INSERT YOUR CODE HERE
 }
 
 /*
- Returns the number of digits in an integer
- PARAM:     number is an integer
- PRE:       0 < number <= INT_MAX
- RETURN:    the number of digits in the number
+ Acquires and returns the maze size.  Since the maze is always a square, all we
+ need to do is find the length of the top row!
+ PARM:      maze_file is a pointer to a filestream
+ PRE:       maze_file is an initialized pointer to a correctly-formatted maze file
+ POST:      maze_file's internal pointer is set to beginning of stream
+ RETURN:    length of the first line of text in the maze file EXCLUDING any EOL characters
+            ('\n' or '\r') and EXCLUDING the string-terminating null ('\0') character.
  */
-int count_digits(int number)
-{
-    if (number < 10) return 1;
-    return count_digits(number / 10) + 1;
+int get_maze_dimension( FILE* maze_file )  {
+
+  int  dimension = 0;
+  char line_buffer[BUFFER];
+
+	dimension = strlen( fgets ( line_buffer, BUFFER, maze_file ) ); //the maze is guaranteed to be square, so only need to check one side -jerry
+
+	/* You don't need to know this.  It 'resets' the file's internal pointer to the
+	   beginning of the file. */
+	fseek( maze_file, 0, SEEK_SET );
+
+    /* Checks if text file was created in Windows and contains '\r'
+	   IF TRUE reduce strlen by 2 in order to omit '\r' and '\n' from each line
+	   ELSE    reduce strlen by 1 in order to omit '\n' from each line */
+  if ( strchr( line_buffer, '\r' ) != NULL ) { //    \r vs \n???
+    // INSERT CODE HERE (1 line)
+      return dimension - 2;
+    // return ...
+  } else {
+      return dimension;
+    // INSERT CODE HERE (1 line)
+    // return ...
+  }
 }
 
 /*
- Returns the length of the specified string.
- PARAM:  string, a pointer to an array of char
- PRE:    the string pointer is not a dangling pointer
- RETURN: the length of the string passed as a parameter
+ Parses and stores maze as a 2D array of maze_cell.  This requires a few steps:
+ 1) Allocating memory for a 2D array of maze_cell, e.g., maze_cell[rows][columns]
+    a) Dynamically allocates memory for 'dimension' pointers to maze_cell, and assign
+	   the memory (case as a double pointer to maze_cell) to maze, which is a
+	   double pointer to maze_cell (this makes the maze[rows] headers)
+	b) For each row of the maze, dynamically allocate memory for 'dimension' maze_cells
+	   and assign it (cast as a pointer to maze_cell) to maze[row]
+ 2) Copying the file to the allocated space
+    a) For each row obtained from the file using fgets and temporarily stored in line_buffer
+	   i) For each of the 'dimension' columns in that row
+	   ii)Assign the character from the file to the struct, and set the struct to unvisited
+ 3) Returns the double pointer called maze.
+ PARAM:  maze_file pointer to an open filestream
+ PARAM:  dimension pointer to an int
+ PRE:    maze_file is a pointer to a correctly-formatted maze file
+ POST:   dimension contains the correct size of the square maze
+ POST:   maze contains a dynamically allocated copy of the maze stored in the maze_file
+ RETURN: maze, a maze_cell double pointer, which points to 'dimension' single pointers
+         to maze_cell, each of which points to 'dimension' maze_cell structs.
  */
-int string_length(char* string)
+maze_cell** parse_maze( FILE* maze_file, int dimension )
 {
-    if (*string == '\0') return 0;
-    return string_length(string + 1) + 1;
+	/* Variables */
+  char        line_buffer[BUFFER];
+  int         row = 0;
+	int         column = 0;
+	maze_cell** maze = NULL; 
+
+  /* Allocates memory for correctly-sized maze */
+  // INSERT CODE HERE (1 line)
+  // maze = ( maze_cell ** ) calloc ... (1 line)
+
+  for ( row = 0; row < dimension; ++row ) {
+    // INSERT CODE HERE (1 line)
+    // maze[row] = ( maze_cell* ) calloc ... (1 line)
+  }
+
+  /* Copies maze file to memory */
+	row = 0;
+  while ( fgets ( line_buffer, BUFFER, maze_file ) ) {
+    for ( column = 0; column < dimension; ++column ) {
+      // INSERT CODE HERE (2 lines)
+      // maze[row][column].character = ...
+      // maze[row][column].visited = ...
+	  }
+    row++;
+  }
+	return maze;
 }
 
-/*
- Determines if a string is a palindrome.  DOES NOT skip spaces!
- For example,
- ""     -> returns 1 (an empty string is a palindrome)
- "the"  -> returns 0
- "abba" -> returns 1
- "Abba" -> returns 0
- "never odd or even" -> returns 0
- For the recursive call, be aware that both parameters can be modified.
- PARAM:  string, a pointer to an array of char
- PARAM:  string_length, the length of the string
- PRE:    the string pointer is not a dangling pointer
- PRE:    string_length is the correct length of the string
- RETURN: IF string is a palindrome
-         THEN 1
-         END 0
+/**
+ Generates all paths through a maze recursively.
+ PARAM:     pointer to a 2D array of all generated paths
+            (pass the 2D array by reference - actual parameter to be modified by function)
+ PARAM:     pointer to number of paths found
+            (pass the integer by reference - actual parameter to be modified by function)
+ PARAM:     pointer to a 2D array of maze_cell
+ PARAM:     dimension of the square maze
+ PARAM:     starting position row
+ PARAM:     starting position column
+ PARAM:     pointer to a string of chars which contains the 'current' path
+ PRE:       maze contains a representation of the maze
+ PRE:       dimension contains the correct dimension of the maze
+ PRE:       row contains a valid row coordinate
+ PRE:       column contains a valid column coordinate
+ PRE:       path contains a sequence of characters (the first time you invoke this
+            function, the passed parameter should be an empty string, e.g., "" (not NULL)
+ POST:      IF current coordinate is not out of maze bounds &&
+               current coordinate is not a wall
+            THEN path contains current coordinate
+ POST:      IF current coordinate is at maze finish line (right boundary)
+            THEN paths contains the path from start to finish.
+ POST:      dereferenced pathset contains all paths found
+ POST:      dereferences numpaths contains the number of paths found
  */
-int is_palindrome(char* string, int string_length)
+//MUST USE RECURSION !!!
+void generate_all_paths( char*** pathsetref, int* numpathsref, maze_cell** maze, int dimension, int row, int column, char* path )
 {
-    if (string_length <= 1) return 1;
-    if (*string != *(string + string_length - 1)) return 0;
-    if (*string == *(string + string_length - 1))
-        return  is_palindrome(string + 1, string_length - 2);
-}
+	/* Variables */
+	int path_length   = 0;
+	char* new_point  = NULL;
+	char* new_path   = NULL;
 
-/*
- Draws a ramp.  The length of the longest rows is specified by the parameter.
- For the recursive call, be aware that both parameters can be modified.
- PARAM:     number, an integer
- PARAM:     buffer, a character array of sufficient length
- PRE:       number >= 1
-            buffer has sufficient length and contains null characters from the buffer
-            address to the end of its length
- POST:      draws a ramp whose height is the specified number into buffer
- RETURN:    the number of characters written into buffer. Don't forget to count your
-            newline characters!
- */
-int draw_ramp(int number, char* buffer)
-{
-    /*strcat(buffer, "\n");
-    draw_row(number, buffer);
+  /* Checks for base cases */
+  if ( // INSERT CODE HERE: Simply return if we hit one of the base cases (there are more than 1)
+       // (remember to delete 1 on the next line, which is here so the incomplete program compiles)
+       1 ) {
+    return;
+	}
 
-    if (number == 1)
-    {
-        if (buffer[0] == '\n') strcpy(buffer, buffer + 1);
-        return 0;
-    }
-    else draw_ramp(number - 1, buffer);
+  /* Otherwise deals with the recursive case.  Pushes the current coordinate onto the path
+	  and checks to see if the right boundary of the maze has been reached
+	  IF   right boundary reached
+	  THEN path is added to the list as a successful path and function returns
+	  ELSE the current location is marked as used and the search continues
+	      in each cardinal direction using a recursive call using these steps:
+		1. get length of path
+		2. allocate space for a larger new path
+		3. allocate space for a new point to add to the path
+		4. assign the value in the maze cell to the new point
+		5. concatenate old path to new path
+		6. concatenate new point to new path */	
+  else {
+	  path_length = strlen( path );
+	  new_path = ( char * ) calloc( path_length + 2, sizeof( char ) );
+	  new_point = ( char * ) calloc( 2, sizeof( char ) );
+	  new_point[0] = maze[row][column].character;
+	  if ( path_length ) {
+		  new_path = strcat( new_path, path );
+	  }
+	  new_path = strcat( new_path, new_point );
+    free(new_point);
 
-    strcat(buffer, "\n");
-    draw_row(number, buffer);
-
-    if (string_length(buffer) == 0) return 0;
-    if (*buffer == '\n') strcpy(buffer, buffer + 1);
-
-    return strlen(buffer);*/
-
-    static int len = 0;
-    if (strlen(buffer) == 0) {//why
-        len = 0;
-    }
-    if (number == 1) {
-        strcat(buffer, "*");
-        len++;
-        //return len;
-    }
-    else {
-        len += draw_row(number, buffer+len);
-        strcat(buffer, "\n");
-        len++;
-        draw_ramp(number - 1, buffer);
-        strcat(buffer, "\n");
-        len++;
-        len += draw_row(number, buffer+len);
-    }
-    return len;
-    /*static int len = 0;
-    static int max;
-
-    if (strlen(buffer) == 0) {//why
-        len = 0;
-        max = number;
-        number = 1;
-    }
-    if (number == max) {
-        
-        len += draw_row(number, buffer + len);
-        
-    }
-    else {
-        len += draw_row(number, buffer + len);
-        strcat(buffer, "\n");
-        len++;
-        draw_ramp(number + 1, buffer);
-        strcat(buffer, "\n");
-        len++;
-        len += draw_row(number, buffer + len);
-
-    }
-    return len;*/
-}
-
-
-/*
- Draws a row of asterisks of the specified length
- For the recursive call, be aware that both parameters can be modified.
- PARAM:  size, an integer
- PARAM:  buffer, a character array of sufficient length
- PRE:    size >= 1
-         buffer has sufficient length and contains null characters from the buffer
-         address to the end of its length
- POST:   draws a row of asterisks of specified length to buffer
- RETURN: the number of characters drawn
- */
-int draw_row(int size, char* buffer)
-{
-    int n;
-    if (1 == size) {
-        buffer[0] = '*';
-        return strlen(buffer);
-    }
-    else {
-        buffer[size - 1] = '*';
-        draw_row(size - 1, buffer);
-    }
-    n = strlen(buffer);
-    return n;
-
-    /*int length = strlen(buffer);
-
-    if(size == 1) {
-        buffer[1] = '*';
+    if ( column == ( dimension - 1 ) ) {
+	    /* 1. Reallocate memory in global paths array to make room
+			    for a new solution string
+			 2. Copy the solution path to the location of new string
+			 3. Increment paths counter */
+	    *pathsetref = ( char** ) realloc ( *pathsetref, ( (*numpathsref) + 1 ) * sizeof( char* ) );
+      (*pathsetref)[*numpathsref] = ( char* ) calloc( strlen( new_path ) + 1, sizeof( char ));
+	    strcpy( (*pathsetref)[*numpathsref], new_path );
+	    (*numpathsref)++;
+      return;
     } else {
-        buffer[size] = '*';
-        draw_row(size - 1, buffer);
+		  /* 1. Mark point as visited
+			   2. Recursively search in each direction using the new path, and the same pathsetref and numpathsref
+			   3. Mark point as unvisited */
+      // INSERT CODE HERE (6 lines)
+      // maze.[row][column].visited = ...
+      // generate_all_paths...
+      // generate_all_paths...
+      // generate_all_paths...
+      // generate_all_paths...
+      // maze.[row][column].visited = ...
+		  return;
     }
-    return strlen(buffer);
-    */
+  }
 }
+
+/*
+ Calculates the cost for a given path.
+ Examples:
+ ""    -> 0
+ "2"   -> 2
+ "871" -> 16
+ PARM:   path_string is a pointer to an array of char
+ PRE:    path_string is a pointer to a null-terminated array of char (a string)
+ RETURN: the 'cost' of the path.
+ */
+int path_cost ( char* path_string )
+{
+  int cost = 0;
+  int i = 0;
+  char* temp = path_string;
+
+  if (temp[0] == '\0') { 
+      cost = 0;
+  }
+  else {
+      while (temp[i] != '\0') {
+          cost+=temp[i];
+          i++; 
+      }
+  }
+  return cost;
+}
+
+/*
+ Writes the shortest path in the paths global variable into the outputbuffer parameter,
+ where the shortest path has the fewest cells.
+ In the event of a tie, use the first 'shortest' path found.
+ Uses this format:
+ "Shortest path: XXXXXXXX"
+ Don't forget to add a null character at the end.
+ PARAM: 2D array containing all paths found
+ PARAM: number of paths in pathset
+ PARAM: outputbuffer is a character array of sufficient length
+ POST:  outputbuffer contains information about the shortest path
+ */
+void construct_shortest_path_info ( char** pathset, int numpaths, char* outputbuffer )
+{
+	// INSERT CODE HERE
+}
+
+/*
+ Writes the cheapest path in the paths global variable into the outputbuffer parameter,
+ where the cheapest path has the lowest sum value of its cells.
+ In the event of a tie, use the first 'cheapest' path found.
+ Uses this format:
+ "Cheapest path: XXXXXXXX
+  Cheapest path cost: 9999"
+ Don't forget to use a newline and to add a null character at the end.
+ Use sprintf to put an integer into a buffer which can then be concatenated using strcat.
+ PARAM: 2D array containing all paths found
+ PARAM: number of paths in pathset
+ PARAM: outputbuffer is a character array of sufficient length
+ POST:  outputbuffer contains information about the cheapest path and its cost
+ */
+void construct_cheapest_path_info ( char** pathset, int numpaths, char* outputbuffer )
+{
+	// INSERT CODE HERE
+}
+
+/* End of file */
